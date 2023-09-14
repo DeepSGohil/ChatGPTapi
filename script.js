252d59914d5227f47c3e282fdd036fd94285e914
DeepSGohil <114739615+DeepSGohil@users.noreply.github.com>
2023-09-15 00:03:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,68 @@
- <script>
-        window.scrollTo(0, document.body.scrollHeight);
+window.scrollTo(0, document.body.scrollHeight);
 
-        const myForm = document.getElementById('myForm');
-        const btn = document.getElementById('search');
-        const msgContainer = document.getElementById('msgContainer');
-        const chatgpt = document.getElementsByClassName('chatgpt');
+const myForm = document.getElementById('myForm');
+const btn = document.getElementById('search');
+const msgContainer = document.getElementById('msgContainer');
+const chatgpt = document.getElementsByClassName('chatgpt');
 
-        myForm.addEventListener('submit', (e) => {
-            e.preventDefault();
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-            const userInput = document.getElementById('msg').value;
-            document.getElementById('msg').value = "";
+    const userInput = document.getElementById('msg').value;
+    document.getElementById('msg').value = "";
 
-            // display the question asked by the user on the screen
-            let myLi = document.createElement('li');
-            myLi.className = "user";
-            myLi.innerText = userInput;
-            msgContainer.appendChild(myLi)
+    // display the question asked by the user on the screen
+    let myLi = document.createElement('li');
+    myLi.className = "user";
+    myLi.innerText = userInput;
+    msgContainer.appendChild(myLi)
 
-
-            const options = {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json',
-                    'X-RapidAPI-Key': '149f2630cbmsh18c06a33edfbeb9p15a4b5jsn30e3ecf21bac',
-                    'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
-                },
-                body: '{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"' + userInput + '"}]}'
-            };
-
-
-            // disable button and add spinner while the data is being fetched through API
-            btn.setAttribute('disabled', true);
-            // btn.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
-
-
-            // add loader for showing that the answer is being generated
-            let li = document.createElement('li');
-            li.className = "chatgpt";
-            li.innerHTML = '<div class="px-3"><div class="snippet" data-title="dot-pulse"><div class="stage"><div class="dot-pulse"></div></div></div></div>';
-            msgContainer.appendChild(li)
-
-
-            fetch('https://openai80.p.rapidapi.com/chat/completions', options)
-                .then(response => response.json())
-                .then(response => {
-                    // document.getElementById('data').innerText = response.choices[0].message.content;
-
-                    // btn.innerHTML = 'Search';
-
-                    chatgpt[chatgpt.length - 1].innerText = response.choices[0].message.content;
-                    window.scrollTo(0, window.length);
-
-                    btn.removeAttribute('disabled');
-                })
-                .catch(err => console.error(err));
+    const url = 'https://open-ai21.p.rapidapi.com/conversation';
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'b50a05bcc2msh100ae93e7d7415bp1a0cadjsnf6f85114db8e',
+            'X-RapidAPI-Host': 'open-ai21.p.rapidapi.com'
+        },
+        body: JSON.stringify({
+            messages: [
+                {
+                    role: 'user',
+                    content: userInput
+                }
+            ],
+            max_token: 500,
+            temperature: 1,
+            web_access: false
         })
+    };
 
-    </script>
+
+    // disable button and add spinner while the data is being fetched through API
+    btn.setAttribute('disabled', true);
+    // btn.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+
+
+    // add loader for showing that the answer is being generated
+    let li = document.createElement('li');
+    li.className = "chatgpt";
+    li.innerHTML = '<div class="px-3"><div class="snippet" data-title="dot-pulse"><div class="stage"><div class="dot-pulse"></div></div></div></div>';
+    msgContainer.appendChild(li)
+
+
+    fetch(url, options)
+        .then(response => response.json())
+        .then(response => {
+            // document.getElementById('data').innerText = response.ChatGPT;
+
+            btn.innerHTML = 'Search';
+
+            chatgpt[chatgpt.length - 1].innerText = response.ChatGPT;
+            window.scrollTo(0, window.length);
+
+            btn.removeAttribute('disabled');
+            // console.log()
+        })
+        .catch(err => console.error(err));
+})
